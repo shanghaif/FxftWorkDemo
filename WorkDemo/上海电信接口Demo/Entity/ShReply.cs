40e5e9f3707597d055b2a36caad9523075f33d28
vagrant2814154894 <2814154894@qq.com>
2017-11-30 19:22:49 +0800

    1

---
+++ WorkDemo/上海电信接口Demo/Entity/ShReply.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelSDK
+{
+    /// <summary>
+    /// 上海电信http应答响应
+    /// </summary>
+    public class ShReply
+    {
+        public string result_msg;
+        /// <summary>
+        /// 应答代码
+        /// </summary>
+        public result_code result_code;
+
+        /// <summary>
+        /// 结果集
+        /// </summary>
+        public List<Recordset> res_recordset;
+
+
+
+    }
+    /// <summary>
+    /// 结果集
+    /// </summary>
+    public class Recordset
+    {
+        public string data;
+    }
+    /// <summary>
+    /// 应答代码
+    /// </summary>
+    public enum result_code
+    {
+        成功 = 0,
+        系统异常 = 1,
+        服务不存在 = 1001,
+        请求参数缺失 = 1002,
+        用户名错误 = 1003,
+        密码错误 = 1004,
+        服务版本错误 = 1005,
+        请求流水重复 = 1006
+    }
+}
---
+++ WorkDemo/上海电信接口Demo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TelSDK;
+
+namespace 上海电信接口Demo
+{
+    /// <summary>
+    ///Http Post+Json  有IP限制  仅供参考
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, object> requestParams = new Dictionary<string, object>();
+            requestParams.Add("service_name", "M2MPakage");
+            dynamic parameter = new
+            {
+                callNbr = "14916205446",
+            };
+            requestParams.Add("service_parameter", parameter);
+            var res = Post(requestParams);
+
+
+            #region 写入文件
+
+            var file = @"D:\Test\test.txt";
+            FileInfo fileInfo = new FileInfo(file);
+            if (!Directory.Exists(fileInfo.DirectoryName))
+            {
+                Directory.CreateDirectory(fileInfo.DirectoryName);
+            }
+            File.WriteAllText(file, res);
+            Console.ReadKey();
+
+            #endregion
+
+
+        }
+
+        static string Post(Dictionary<string, object> requestParams)
+        {
+            Dictionary<string, object> body = new Dictionary<string, object>();
+            var msgId = Guid.NewGuid().ToString().Replace("-", "");
+            body.Add("msgId", msgId);
+            body.Add("version_id", "1.5");//
+            body.Add("consumer", "saige");//用户名
+            body.Add("password", "123456");//
+            body.Add("test_flag", "1");//0 生产数据
+            body.Add("reqTime", DateTime.Now.ToString());
+            foreach (var item in requestParams)
+            {
+                body.Add(item.Key, item.Value);
+            }
+
+            string requestUrl = $"http://101.95.48.192:8080/tsp-api/TspService";
+            HttpClient httpClient = new HttpClient();
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(body));
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var task = httpClient.PostAsync(requestUrl, httpContent).Result;
+            string res = null;
+            if (task.StatusCode == HttpStatusCode.OK)
+            {
+                res = task.Content.ReadAsStringAsync().Result;
+            }
+            Console.WriteLine(res);
+          
+            return res;
+        }
+
+    }
+}

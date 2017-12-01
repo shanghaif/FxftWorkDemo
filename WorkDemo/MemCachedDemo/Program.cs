4f2fd5f824cea37710529b4f16da371d3fd59003
vagrant2814154894 <2814154894@qq.com>
2017-12-01 14:59:55 +0800

    1

---
+++ WorkDemo/MemCachedDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemCachedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //清除所有的缓存数据
+            MemCachedManager.cache.FlushAll();
+
+            var v1 = MemCachedManager.cache.Add("a", "123");
+            var res1 = MemCachedManager.cache.Get("a");
+            Console.WriteLine("第一次Add返回值{0},结果{1}", v1, res1);
+
+            var v2 = MemCachedManager.cache.Add("a", "456");
+            var res2 = MemCachedManager.cache.Get("a");
+            Console.WriteLine("第二次Add返回值{0},结果{1}", v2, res2);
+
+
+            var v3 = MemCachedManager.cache.Set("a", "789");
+            var res3 = MemCachedManager.cache.Get("a");
+            Console.WriteLine("Set返回值{0}，结果{0}", res3);
+
+
+            var v4 = MemCachedManager.cache.Replace("a", "123");
+            var res4 = MemCachedManager.cache.Get("a");
+            Console.WriteLine("replace返回值{0}，结果{0}", res4);
+
+            var v5 = MemCachedManager.cache.Replace("b", "123");
+            Console.WriteLine("replace返回值{0}", v5);
+
+            MemCachedManager.cache.Set("b", "456");
+            MemCachedManager.cache.Delete("b");
+            var v6 = MemCachedManager.cache.KeyExists("b");
+            Console.WriteLine("Delete之后返回值{0}", v6);
+        }
+    }
+}

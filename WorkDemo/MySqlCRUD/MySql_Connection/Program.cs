0d66c74331e3df60bd7896924bdff29e02896e05
vagrant2814154894 <2814154894@qq.com>
2017-12-01 15:55:50 +0800

    1

---
+++ WorkDemo/MySqlCRUD/MySql_Connection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MySql_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string constr = "server=localhost;User Id=sa;password=123456;Database=datasync";
+            MySqlConnection mycon = new MySqlConnection(constr);
+            mycon.Open();
+            MySqlCommand mycmd = new MySqlCommand("insert into datasyn_log(comId) values('90')", mycon);
+            mycmd.ExecuteNonQuery();
+            //mycmd =new MySqlCommand("update datasyn_log set comId='100' where comId='90'",mycon);
+            //mycmd.ExecuteNonQuery();
+            mycmd = new MySqlCommand("delete from datasyn_log  where comId='100'", mycon);
+            mycmd.ExecuteNonQuery();
+
+            //if (mycmd.ExecuteNonQuery() > 0)
+            //{
+            //    Console.WriteLine("数据插入成功！");
+            //}
+            //Console.ReadLine();
+            mycon.Close();
+        }
+    }
+}

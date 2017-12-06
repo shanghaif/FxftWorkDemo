a8caf065242daaf9ad471b0a0342925ae07edf42
vagrant2814154894 <2814154894@qq.com>
2017-12-06 11:20:40 +0800

    1

--- WorkDemo/NLogUsingDemo/Program.cs
+++ WorkDemo/NLogUsingDemo/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace NLogUsingDemo
 {
+    /// <summary>
+    /// 注意：NLog.config文件要能够复制才能读到配置
+    /// </summary>
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+
+            logger.Trace("race Messag");
+            logger.Debug("Debug Message");
+            logger.Info("Info Message");
+            logger.Error("Error Message");
+        
         }
     }
 }

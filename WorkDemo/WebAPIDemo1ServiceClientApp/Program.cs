2cf7ce88bb5c742d5c44a15f6f8d89bd1c20ea6e
vagrant2814154894 <2814154894@qq.com>
2017-12-15 17:45:22 +0800

    1

---
+++ WorkDemo/WebAPIDemo1ServiceClientApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using WebAPIDemo1.Models;
+
+namespace WebAPIDemo1ServiceClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client app, wait for service");
+            Console.ReadLine();
+           ReadArraySample().Wait();
+            // ReadSampleXml().Wait();
+            // AddSample().Wait();
+            // UpdateSample().Wait();
+            // DeleteSample().Wait();
+            //UpdateWithErrorSample().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task ReadArraySample()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:11825");
+            string response = await client.GetStringAsync("/api/BookChapters");
+            Console.WriteLine(response);
+            var serializer = new JavaScriptSerializer();
+            BookChapter[] chapters =
+              serializer.Deserialize<BookChapter[]>(response);
+
+            foreach (BookChapter chapter in chapters)
+            {
+                Console.WriteLine(chapter.Title);
+            }
+        }
+    }
+}
